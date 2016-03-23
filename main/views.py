5e13c1b1b4ed2543cd0ae96e3ff14c92bada3b89
polmes <pol.developer1@gmail.com>
2016-03-23 18:33:47 +0100

    better order

--- main/views.py
+++ main/views.py
@@ -5,8 +5,8 @@ from .models import Station
 # Create your views here.
 def index(request):
 	stations = Station.objects.all()
-	template = loader.get_template('main/index.html')
 	context = {
 		'stations': stations,
 	}
+	template = loader.get_template('main/index.html')
 	return HttpResponse(template.render(context, request))
