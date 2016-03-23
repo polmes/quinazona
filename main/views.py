886174a732eb4135a0214860101a69ac596e6d1d
polmes <pol.developer1@gmail.com>
2016-03-23 18:30:58 +0100

    started work on renderer

--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Station
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>Welcome to QuinaZona.cat!</h1>")
+	stations = Station.objects.all()
+	template = loader.get_template('main/index.html')
+	context = {
+		'stations': stations,
+	}
+	return HttpResponse(template.render(context, request))
